18f6a833e7187bf63e7f55e99177a19865e33791
Hossam <hossamhamdy117@gmail.com>
2022-05-20 22:47:54 +0200

    solve 12 problem

--- 02 Problem Solving/Level B/CF816-D2-B.cpp
+++ 02 Problem Solving/Level B/CF816-D2-B.cpp
@@ -1,34 +1,56 @@
-#include<iostream>
-#include<algorithm>
-#include<vector>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main()
-{
-    const int MAX {200001};
-    int n, k, q, s, e;
-    scanf("%d%d%d", &n,&k,&q);
-    vector<int> v(MAX,0);
-    while(n--)
-    {
-        scanf("%d%d", &s,&e);
-        v[s]++;
-        v[e+1]--;
+typedef pair<int,int> CELL;
+#define x first
+#define y second
+#define ll long long
+
+void fast_IO(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+}
+void readFile(){
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+}
+
+
+int main(){
+    fast_IO();
+
+    const int MAX {200005};
+    int freq[MAX] = {0};
+
+    int n,k,q;
+    cin >> n >> k >> q;
+
+    // 1) mark Intervals
+    while(n--){
+        int a,b;
+        cin >> a >> b;
+        freq[a]++;
+        freq[b+1]--;
     }
 
-    for(int i=1; i<MAX; i++)
-        v[i] = v[i] + v[i-1];
+    // 2) calculate frequencies
+    for(int i = 1; i < MAX; ++i)
+        freq[i] += freq[i-1];
 
-    for(int i=1; i<MAX; i++)
-        v[i] = (k<=v[i]);
+    // 3) validate if(admissible)
+    for(int i = 0; i < MAX; ++i)
+        (freq[i] < k) ? freq[i] = 0 : freq[i] = 1;
 
-    for(int i=1; i<MAX; i++)
-        v[i] = v[i] + v[i-1];
+    // 4) accumulate frequencies
+    for(int i = 1; i < MAX; ++i)
+        freq[i] += freq[i-1];
 
-    while(q--)
-    {
-        scanf("%d%d", &s,&e);
-        printf("%d\n", v[e]-v[s-1]);
+
+    // 5) answer queries
+    while(q--){
+        int a,b;
+        cin >> a >> b;
+        cout << freq[b] - freq[a-1] << "\n";
     }
 
     return 0;
