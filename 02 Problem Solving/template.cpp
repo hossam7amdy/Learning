c92b02c94fd110fb3a5f0c8ced7ad3c42766288c
Hossam <hossamhamdy117@gmail.com>
2022-05-28 10:07:52 +0200

    add new 2 problems

---
+++ 02 Problem Solving/template.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef pair<int,int> CELL;
+#define x first
+#define y second
+#define ll long long
+
+void fast_IO(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+}
+void readFile(){
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+}
+
+
+int main(){
+    fast_IO();
+
+
+    return 0;
+}
