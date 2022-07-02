3488aad463fa5191e21433509dfa3d6e20e8e788
Hossam <hossamhamdy117@gmail.com>
2022-07-02 11:07:48 +0200

    week 2

--- 10 JavaScript/01-Fundamentals-Part-1/script.js
+++ 10 JavaScript/01-Fundamentals-Part-1/script.js
@@ -173,5 +173,4 @@ const tip = 50 <= bill && bill <= 300 ? 0.15 * bill : 0.2 * bill;
 console.log(
   `The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}.`
 );
-
 //////////////////////////////////////////////
